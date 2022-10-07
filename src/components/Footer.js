a4c479634872d8070bb1007c1e39870fd6bbae03
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-07 19:37:05 +1100

    footer.js basic setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import github from '../components/assets/footer/github.png';
+import slack from '../components/assets/footer/slack-overflow.png';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer id='footer'>
+      <div className='profileLinks'>
+        <a
+          href='https://github.com/michaelclancy90'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img src={github} alt='github' />
+        </a>
+        <a
+          href='https://stackoverflow.com/users/19812568/michael-clancy'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img src={slack} alt='slack' />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
