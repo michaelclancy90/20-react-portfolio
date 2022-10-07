b4795c68d05cd791c04a7155f20952f760fb678c
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-07 18:50:21 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './components/pages/AboutMe.js';
+import Contact from './components/pages/Contact.js';
+import Portfolio from './components/pages/Portfolio.js';
+import Resume from './components/pages/Resume.js';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<AboutMe />} />
+          <Route path='/Portfolio' element={<Portfolio />} />
+          <Route path='/Contact' element={<Contact />} />
+          <Route path='/Resume' element={<Resume />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
